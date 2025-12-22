714c24faab585de784765d3dd0691a27c9c1a757
Caio Vasconcelos <caiovasconcelos01@live.com>
2025-12-22 10:49:21 -0300

    :sparkles: feat: FilterStatus,Logos, Enum, FilterStatus

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Image, View, TouchableOpacity, Text } from "react-native"
+
+import { styles } from "./styles"
+import { Button } from "@/Components/Button"
+import { Input } from "@/Components/Input"
+import { Filter } from "@/Components/Filter"
+import { FilterStatus } from "@/types/FilterStatus"
+
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+
+
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
+
+      <View style={styles.form}
+      >
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Adicionar" />
+      </View >
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {
+            FILTER_STATUS.map((status) =>
+              <Filter key={status} status={status} isActive />
+            )
+          }
+          <TouchableOpacity style={styles.clearButtom}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
+}
